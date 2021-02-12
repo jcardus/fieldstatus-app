1d0d85dcba25a4f49a24091c1df5e7f66c161f08
jcardus <asklocation.net@gmail.com>
2021-02-12 23:53:27 +0000

    adding firebase auth

--- src/main.ts
+++ src/main.ts
@@ -23,10 +23,35 @@ import '@ionic/vue/css/display.css';
 /* Theme variables */
 import './theme/variables.css';
 
+// Firebase App (the core Firebase SDK) is always required and must be listed first
+import firebase from "firebase/app";
+// If you are using v7 or any earlier version of the JS SDK, you should import firebase using namespace import
+// import * as firebase from "firebase/app"
+
+// If you enabled Analytics in your project, add the Firebase SDK for Analytics
+import "firebase/analytics";
+
+// Add the Firebase products that you want to use
+import "firebase/auth";
+import "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBe2gsMZaCPt_9gFX55ADod2S4T2wFr1lE",
+  authDomain: "fleetstat.us",
+  projectId: "fieldstatus-c4f7c",
+  storageBucket: "fieldstatus-c4f7c.appspot.com",
+  messagingSenderId: "1065922007703",
+  appId: "1:1065922007703:web:2d71c0fb076fe127e6cfd2",
+  measurementId: "G-SQ6YG8E911"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+
 const app = createApp(App)
   .use(IonicVue)
   .use(router);
-  
+
 router.isReady().then(() => {
   app.mount('#app');
 });
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,14 +1,58 @@
 <template>
   <div>
+    <div id="firebaseui-auth-container" />
+    <div id="loader">
+      Loading...
+    </div>
   </div>
 </template>
 
 <script>
+
+import * as firebaseui from 'firebaseui'
+import firebase from 'firebase/app'
+import 'firebaseui/dist/firebaseui.css'
+
 export default {
-  name: "Login"
+  name: "Login",
+  mounted() {
+    // Initialize the FirebaseUI Widget using Firebase.
+    const ui = new firebaseui.auth.AuthUI(firebase.auth())
+    const uiConfig = {
+      callbacks: {
+        signInSuccessWithAuthResult (/* authResult, redirectUrl */) {
+          // User successfully signed in.
+          // Return type determines whether we continue the redirect automatically
+          // or whether we leave that to developer to handle.
+          return true
+        },
+        uiShown () {
+          // The widget is rendered.
+          // Hide the loader.
+          document.getElementById('loader').style.display = 'none'
+        }
+      },
+      // Will use popup for IDP Providers sign-in flow instead of the default, redirect.
+      signInFlow: 'popup',
+      signInSuccessUrl: '/',
+      signInOptions: [
+        // Leave the lines as is for the providers you want to offer your users.
+        firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+        {
+          provider: 'apple.com'
+        },
+        firebase.auth.EmailAuthProvider.PROVIDER_ID
+      ],
+      // Terms of service url.
+      tosUrl: '<your-tos-url>',
+      // Privacy policy url.
+      privacyPolicyUrl: '<your-privacy-policy-url>'
+    }
+    // The start method will wait until the DOM is loaded.
+    ui.start('#firebaseui-auth-container', uiConfig)
+  }
 }
 </script>
 
-<style scoped>
-
+<style lang="scss">
 </style>
